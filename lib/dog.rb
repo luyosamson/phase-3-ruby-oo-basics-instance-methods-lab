a3a019c7f8c498771dc832d541ace18d185019e0
luyosamson <luyosamson@gmail.com>
2022-10-19 18:02:29 +0300

    Done

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+    #class body
+    #instance method definition
+
+def bark
+    puts "Woof!"
+
+def sit
+    puts "The Dog is sitting"
+
+end
+
+    end
+end
+fido=Dog.new
+fido.bark
+Sat=Dog.new
+Sat.sit
+
+
+
---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+
+    def talk
+        puts  "Hello World!"
+
+    end
+
+     def walk
+            puts "The Person is walking"
+    end
+end
+audio=Person.new
+audio.talk
+mobile=Person.new
+mobile.walk
